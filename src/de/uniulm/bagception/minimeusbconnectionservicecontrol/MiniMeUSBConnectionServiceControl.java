4efca0dc070da072d22ccfc9a3ca4e5d7373a25a
Steffen Herrdum <steffen.herrdum@uni-ulm.de>
2013-11-27 12:27:32 +0100

    disabled tonegenerator, edited stuff

--- src/de/uniulm/bagception/minimeusbconnectionservicecontrol/MiniMeUSBConnectionServiceControl.java
+++ src/de/uniulm/bagception/minimeusbconnectionservicecontrol/MiniMeUSBConnectionServiceControl.java
@@ -1,7 +1,12 @@
 package de.uniulm.bagception.minimeusbconnectionservicecontrol;
 
+import java.util.HashMap;
+
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.graphics.Color;
 import android.media.AudioManager;
 import android.media.ToneGenerator;
@@ -10,6 +15,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.TextView;
 import de.philipphock.android.lib.logging.LOG;
 import de.philipphock.android.lib.services.ServiceUtil;
@@ -25,8 +31,13 @@ public class MiniMeUSBConnectionServiceControl extends Activity implements Servi
 	private USBConnectionActor usbConnectionActor;
 	public static final String USB_CONNECTION_BROADCAST_RFIDSCAN = "de.uniulm.bagception.service.broadcast.usbconnection.rfidscan";
 
-	
-
+	private ToneGenerator tg = new ToneGenerator(AudioManager.STREAM_MUSIC, 100);
+	private boolean clearTagList = true;		// if true, taglist will be deleted after every scantime
+    private HashMap<String, Long> hashMapIdList = new HashMap<String, Long>();	// maps each unique tagID with scanning timestamp
+    private long timeDiff = 5000;	// 
+    int tagInTone = tg.TONE_PROP_ACK;
+    int tagOutTone = tg.TONE_PROP_NACK;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -50,18 +61,48 @@ public class MiniMeUSBConnectionServiceControl extends Activity implements Servi
 		Intent startServiceIntent = new Intent(ServiceNames.RFID_SERVICE);
 		startService(startServiceIntent);	
 		
+		{
+            IntentFilter f = new IntentFilter();
+            f.addAction(BagceptionBroadcastContants.BROADCAST_RFID_TAG_FOUND);
+            registerReceiver(rfidTagReceiver, f);
+		}
+		
 		
 	}
 	@Override
 	protected void onPause() {
 		observationActor.unregister(this);
 		usbConnectionActor.unregister(this);
+        unregisterReceiver(rfidTagReceiver);
 		super.onPause();
 	}
+
+	
+	// for received broadcasts with rfid tagIDs...
+	BroadcastReceiver rfidTagReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			String id = intent
+					.getStringExtra(BagceptionBroadcastContants.BROADCAST_RFID_TAG_FOUND);
+			if(hashMapIdList.containsKey(id)){
+				log("id already known...");
+				// if tag was scanned after timeDiff period, tagID will be removed
+				// else, timestamp will be updated for tagID
+				if(System.currentTimeMillis() - hashMapIdList.get(id) > timeDiff){
+					hashMapIdList.remove(id);
+					tg.startTone(tagOutTone);
+				}else{
+					hashMapIdList.put(id, System.currentTimeMillis());
+				}
+			}else{
+				hashMapIdList.put(id, System.currentTimeMillis());
+				tg.startTone(tagInTone);
+			}
+		}
+
+	};
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		
 		return true;
 	}
 
@@ -102,13 +143,37 @@ public class MiniMeUSBConnectionServiceControl extends Activity implements Servi
 		v.setTextColor(Color.BLUE);
 	}
 	
+	
 	public void onScanButtonClick(View v){
+		// clears the hashMapList with tagIDs, if option is set
+		if(clearTagList){
+			hashMapIdList.clear();
+		}
+		// sends scan intent
 		Intent i = new Intent();
 		i.setAction(USB_CONNECTION_BROADCAST_RFIDSCAN);
-		
 		sendBroadcast(i);
 	}
 	
+	public void onCheckboxClicked(View v){
+		CheckBox checkBox = (CheckBox) v;
+		if(checkBox.isChecked()){
+			clearTagList = true;
+		}else{
+			clearTagList = false;
+		}
+			
+	}
+
+	
+	
+	
+	private void log(String string) {
+		Log.d("MiniMeUSBConnectionServiceControl", string);
+	}
+
+
+
 	public void onStartStopService(View v){
 		Button startStopBtn = (Button) findViewById(R.id.startStopService);
 		startStopBtn.setEnabled(false);
@@ -121,6 +186,7 @@ public class MiniMeUSBConnectionServiceControl extends Activity implements Servi
 			stopService(startServiceIntent);
 		}
 	}
+	
 
 
 
